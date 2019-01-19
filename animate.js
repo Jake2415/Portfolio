d71e27d73445a6c172744b5002e389da63a84662
Unknown <jrubalcava@g.ucla.edu>
2019-01-18 20:49:13 -0800

    Quick Changes
    removed carousels

--- animate.js
+++ animate.js
@@ -1,1 +1,53 @@
-ScrollReveal().reveal('.project-img');
+//Scroll Reveal//
+(function scrollReveal() {
+    window.sr = ScrollReveal();
+    
+    sr.reveal('.project-img', {
+      delay      : 350,
+      duration   : 350,
+      distance   : '20px',
+      easing     : 'ease-out',
+      origin     : 'left',
+      reset      : false,
+      scale      : 1,
+      viewFactor : 1,
+    }, 150);
+    
+    sr.reveal('.project-text', {
+      delay      : 350,
+      duration   : 350,
+      scale      : 1,
+      distance   : '20px',
+      origin     : 'right',
+      reset      : false,
+      easing     : 'ease-out',
+      viewFactor : 1,
+    }, 75);
+  })();
+
+
+
+  (function scrollReveal() {
+    window.sr = ScrollReveal();
+    
+    sr.reveal('.title', {
+      duration   : 600,
+      distance   : '20px',
+      easing     : 'ease-out',
+      origin     : 'bottom',
+      reset      : false,
+      scale      : 1,
+      viewFactor : 0,
+      afterReveal  : revealChildren,
+    }, 150);
+    
+      var revealChildren = sr.reveal('.', {
+      duration   : 1000,
+      scale      : 1,
+      distance   : '20px',
+      origin     : 'bottom',
+      reset      : false,
+      easing     : 'ease-out',
+      viewFactor : 1,
+    }, 75);
+  })();
